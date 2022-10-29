fdb1e755c8610ec67dd9d4e101644152dc7bd74e
Mark Dominus <mjd@leapyear.io>
2022-10-29 18:48:43 -0400

    WIP

--- src/Ezpr.hs
+++ src/Ezpr.hs
@@ -62,6 +62,10 @@ instance (Eq a, Num a) => Fractional (Ezpr a) where
     d = denominator r
     intCon = Con . fromInteger
 
+isEmpty :: Op -> Ezpr a -> Bool
+isEmpty o (Oper o' ([], [])) | o == o' = True
+isEmpty _ _ = False
+
 -- if the two bags (a, b) have any elements in common,
 -- remove the common elements from both
 -- for example ([1,2,3,4], [2, 4, 4, 6]) -> ([1,3], [4,6])
@@ -92,12 +96,19 @@ arithRight o (Oper o1 (lt1, rt1)) e2 | o == o1 = Oper o (lt1, e2 : rt1)
 arithRight o e1 (Oper o2 (lt2, rt2)) | o == o2 = negate $ arithRight o (Oper o2 (lt2, rt2)) e1
 arithRight o e1 e2 = Oper o ([e1], [e2])
 
-mkOper :: Eq a => Op -> ([Ezpr a], [Ezpr a]) -> Ezpr a
-mkOper op = Oper op . removeDups
+mkOper :: (Num a, Eq a) => Op -> ([Ezpr a], [Ezpr a]) -> Ezpr a
+mkOper op = Oper op . (both $ removeIdentities op) . removeDups
+ where
+  both f (a, b) = (f a, f b)
+  -- removeIdentities :: (Eq a, Num a) => Op -> [Ezpr a] -> [Ezpr a]
+  removeIdentities SUM = filter (/= (Con 0))
+  removeIdentities MUL = filter (/= (Con 1))
 
 addEzprs :: (Eq a, Num a) => Ezpr a -> Ezpr a -> Ezpr a
 addEzprs e (Con 0) = e
 addEzprs (Con 0) e = e
+addEzprs e1 e2 | isEmpty MUL e2 = e1 + Con 1
+addEzprs e1 e2 | isEmpty MUL e1 = e2 + Con 1
 Oper SUM (lt1, rt1) `addEzprs` Oper SUM (lt2, rt2) =
   mkOper SUM (lt1 ++ lt2, rt1 ++ rt2)
 Oper SUM (lt1, rt1) `addEzprs` e2 = mkOper SUM (e2 : lt1, rt1)
@@ -107,10 +118,12 @@ e1 `addEzprs` e2 = mkOper SUM ([e1, e2], [])
 subEzprs :: (Eq a, Num a) => Ezpr a -> Ezpr a -> Ezpr a
 subEzprs e (Con 0) = e
 subEzprs (Con 0) e = negate e
+subEzprs e1 e2 | isEmpty MUL e2 = e1 - Con 1
+subEzprs e1 e2 | isEmpty MUL e1 = Con 1 - e2
 Oper SUM (lt1, rt1) `subEzprs` Oper SUM (lt2, rt2) =
   mkOper SUM (lt1 ++ rt2, rt1 ++ lt2)
 Oper SUM (lt1, rt1) `subEzprs` e2 = mkOper SUM (lt1, e2 : rt1)
-e1 `subEzprs` Oper SUM (lt2, rt2) = mkOper SUM (lt2, e1 : rt2)
+e1 `subEzprs` Oper SUM (lt2, rt2) = mkOper SUM (e1 : rt2, lt2)
 e1 `subEzprs` e2 = mkOper SUM ([e1], [e2])
 
 mulEzprs :: (Eq a, Num a) => Ezpr a -> Ezpr a -> Ezpr a
@@ -120,6 +133,7 @@ mulEzprs (Con 1) e = e
 mulEzprs e (Con 1) = e
 mulEzprs (Con (-1)) e = negate e
 mulEzprs e (Con (-1)) = negate e
+mulEzprs e1 e2 | isEmpty SUM e1 || isEmpty SUM e2 = Con 0
 Oper MUL (lt1, rt1) `mulEzprs` Oper MUL (lt2, rt2) =
   mkOper MUL (lt1 ++ lt2, rt1 ++ rt2)
 Oper MUL (lt1, rt1) `mulEzprs` e2 = mkOper MUL (e2 : lt1, rt1)
@@ -129,8 +143,9 @@ e1 `mulEzprs` e2 = mkOper MUL ([e1, e2], [])
 divEzprs :: (Eq a, Num a) => Ezpr a -> Ezpr a -> Ezpr a
 divEzprs e (Con 1) = e
 divEzprs e (Con (-1)) = negate e
+divEzprs e1 _ | isEmpty SUM e1 = Con 0 -- e2?
 Oper MUL (lt1, rt1) `divEzprs` Oper MUL (lt2, rt2) =
-  mkOper MUL (lt1 ++ lt2, rt1 ++ rt2)
-Oper MUL (lt1, rt1) `divEzprs` e2 = mkOper MUL (e2 : lt1, rt1)
-e1 `divEzprs` Oper MUL (lt2, rt2) = mkOper MUL (e1 : lt2, rt2)
-e1 `divEzprs` e2 = mkOper MUL ([e1, e2], [])
+  mkOper MUL (lt1 ++ rt2, lt2 ++ rt1)
+Oper MUL (lt1, rt1) `divEzprs` e2 = mkOper MUL (lt1, e2 : rt1)
+e1 `divEzprs` Oper MUL (lt2, rt2) = mkOper MUL (e1 : rt2, lt2)
+e1 `divEzprs` e2 = mkOper MUL ([e1], [e2])
